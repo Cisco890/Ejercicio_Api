2a1f906d51f188a5706904e281758275b98817d9
Juan Francisco Martínez <jfmartinez890@gmail.com>
2025-04-01 15:21:31 -0600

    incidentes por medio del id

--- api.js
+++ api.js
@@ -20,35 +20,49 @@ app.get('/', (req, res) => {
 }
 );
 
+//Se obtienen los incidentes 
 app.get('/incidents', (req, res) => {
     res.send(incidents);
 });
 
+// se hace un post para los incidentes
 app.post('/incidents', (req, res) => {
-    const { reporter, description, status } = req.body;
+    const { reporter, description } = req.body;
 
-    if (!reporter || !description || !status) {
-        return res.status(400).json({ error: 'Son requeridos: reporter, description y status' });
+    if (!reporter || !description) {// si el incidente que se quiere agregar no tiene uno de estos campos, da error. 
+        return res.status(400).json({ error: 'Son requeridos: reporter, description' });
     }
 
-    const newIncident = {
+    const newIncident = {// la "estructura" de los incidentes
         id: incidents.length + 1,
         reporter,
         description,
-        status,
+        status: "pendiente",
         created_at: new Date().toISOString()
     };
 
-    incidents.push(newIncident); 
+    incidents.push(newIncident); // push a los incidentes
 
     res.status(201).json({
-        message: 'Nuevo incidente creado con éxito',
+        message: 'Nuevo incidente creado con éxito',// mensaje de confirmación de la creación de los incidentes 
         incident: newIncident
     });
 });
 
+//Se obtienen los incidentes por medio del id
+app.get('/incidents/:id', (req, res) => {
+    const id = parseInt(req.params.id); 
 
-const incidents = [
+    const incident = incidents.find(inc => inc.id === id); // se busca por el id
+
+    if (!incident) {
+        return res.status(404).json({ error: 'Incidente no encontrado' });// si el id no existe se muestra este mensaje
+    }
+
+    res.json(incident); 
+});
+
+const incidents = [// incidentes guardados 
     {
         id: 1,
         reporter: "Lucía Fernández",
